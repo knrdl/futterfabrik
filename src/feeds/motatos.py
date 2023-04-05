c6ed72b38b2a50f2c5a047161a0e66e694e0eb8f
knrdl <35548889+knrdl@users.noreply.github.com>
2023-04-05 22:33:37 +0200

    fix: improve crawler

--- src/feeds/motatos.py
+++ src/feeds/motatos.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import html
 
 from feedgen import Feed
 
 TITLE = 'Motatos'
 HOMEPAGE = 'https://www.motatos.de/neu-im-shop'
+
+_ = html.escape
 
 def generate():
     feed = Feed(
@@ -19,6 +22,15 @@ def generate():
         url = 'https://www.motatos.de' + link.get('href')
         title = link.find('span', {'class': 'label'}).text
         price = link.find('div', {'class': 'product-item-price'}).text.removesuffix('*')
+        pic_url = link.find('img').get('src')
+        if pic_url:
+            pic_url = pic_url.split('?')[0]
+        info2 = link.find('span', {'class': 'brand-and-weight'}).text
+        info3 = link.find('span', {'class': 'comparison-price'}).text
 
-        feed.add_item(title=f'{title} ({price})', body=link.text, url=url, unique_id=url)
+        feed.add_item(title=f'{title} ({price})', body=f'''
+            <h1>{_(title)} ({_(price)})</h1>
+            <p>{_(info2)} {_(info3)}</p>
+            <img src="{_(pic_url)}">
+        ''', url=url, unique_id=url)
     return feed
